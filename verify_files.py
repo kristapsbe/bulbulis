24d7f2a1a406d7f4f0296fc856647e13106ed375
Kristaps <kbebris+git@gmail.com>
2025-03-10 15:24:00 +0200

    shortened verif script

--- verify_files.py
+++ verify_files.py
@@ -23,6 +23,9 @@ for f in files.values():
     dates += list(f.keys())
 dates = sorted(list(set(dates)))
 
+bands = {
+    'B02', 'B03', 'B04', 'B08', 'SCL'
+}
 
 ct = 0
 for d in dates:
@@ -31,43 +34,12 @@ for d in dates:
     for ki, vi in files.items():
         if d in vi:
             fj = vi[d]
-            if "SCL" in fj:
-                try:
-                    rasterio.open(fj["SCL"], driver="JP2OpenJPEG").read(1)
-                    os.rename(fj["SCL"], f"data_verified/{fj["SCL"].split("/")[-1]}")
-                except BaseException as e:
-                    if os.path.isfile(fj["SCL"]):
-                        print(e, "REMOVED", fj["SCL"])
-                        os.remove(fj["SCL"])
-            if "B08" in fj:
-                try:
-                    rasterio.open(fj["B08"], driver="JP2OpenJPEG").read(1)
-                    os.rename(fj["B08"], f"data_verified/{fj["B08"].split("/")[-1]}")
-                except BaseException as e:
-                    if os.path.isfile(fj["B08"]):
-                        print(e, "REMOVED", fj["B08"])
-                        os.remove(fj["B08"])
-            if "B04" in fj:
-                try:
-                    rasterio.open(fj["B04"], driver="JP2OpenJPEG").read(1)
-                    os.rename(fj["B04"], f"data_verified/{fj["B04"].split("/")[-1]}")
-                except BaseException as e:
-                    if os.path.isfile(fj["B04"]):
-                        print(e, "REMOVED", fj["B04"])
-                        os.remove(fj["B04"])
-            if "B03" in fj:
-                try:
-                    rasterio.open(fj["B03"], driver="JP2OpenJPEG").read(1)
-                    os.rename(fj["B03"], f"data_verified/{fj["B03"].split("/")[-1]}")        
-                except BaseException as e:
-                    if os.path.isfile(fj["B03"]):
-                        print(e, "REMOVED", fj["B03"])
-                        os.remove(fj["B03"])
-            if "B02" in fj:
-                try:
-                    rasterio.open(fj["B02"], driver="JP2OpenJPEG").read(1)
-                    os.rename(fj["B02"], f"data_verified/{fj["B02"].split("/")[-1]}")
-                except BaseException as e:
-                    if os.path.isfile(fj["B02"]):
-                        print(e, "REMOVED", fj["B02"])
-                        os.remove(fj["B02"])
+            for b in bands:
+                if b in fj:
+                    try:
+                        rasterio.open(fj[b], driver="JP2OpenJPEG").read(1)
+                        os.rename(fj[b], f"data_verified/{fj[b].split("/")[-1]}")
+                    except BaseException as e:
+                        if os.path.isfile(fj[b]):
+                            print(e, "REMOVED", fj[b])
+                            os.remove(fj[b])
