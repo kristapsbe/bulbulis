e0cd25c9addce68172615a65a6f5d46819a41933
Kristaps <kbebris+git@gmail.com>
2025-04-21 15:09:09 +0300

    dl

--- make_frames.py
+++ make_frames.py
@@ -1,8 +1,3 @@
-# zoom both T34 and T35 (0.2 ?)
-# https://docs.scipy.org/doc/scipy/reference/generated/scipy.ndimage.zoom.html#scipy.ndimage.zoom
-# then rotate T35
-# https://docs.scipy.org/doc/scipy/reference/generated/scipy.ndimage.rotate.html
-
 import os
 import numpy as np
 import rasterio
@@ -11,6 +6,29 @@ import matplotlib.image
 from pathlib import Path
 from scipy.ndimage import zoom, rotate
 
+
+target_year = 2018
+if len(sys.argv) > 1:
+    target_year = int(sys.argv[1])
+prev_year = start_year-1
+
+files = {}
+
+for f in os.listdir("/home/kristaps/Projs/bulbulis/data_verified"):
+    if ".jp2" in f:
+        parts = f.split("_")
+        if parts[0] not in files:
+            files[parts[0]] = {}
+        if parts[1] not in files[parts[0]]:
+            files[parts[0]][parts[1]] = {}
+        files[parts[0]][parts[1]][parts[2]] = f"/home/kristaps/Projs/bulbulis/data_verified/{f}"
+
+dates = []
+for f in files.values():
+    dates += list(f.keys())
+dates = sorted(list(set(dates)))
+
+dates = [d for d in dates if d[:4] == str(target_year) or d[:6] in set([f"{prev_year}{str(i).zfill(2)}" for i in range(8, 13)])]
 
 gain = 2
 deg = 5.1
@@ -54,84 +72,51 @@ right_offsets = {
     'T35UNB': (3, 2),
 }
 
-# TODO: this makes it easier to line up angles - may want to get rid of it since it makes rotation a lot more expensive
+# TODO: this makes it easier to line up angles - get rid of it
 right = np.dstack((np.zeros((bc_h, bc_w)), np.zeros((bc_h, bc_w)), np.zeros((bc_h, bc_w))))
 composite = np.dstack((np.zeros((t_h, t_w)), np.zeros((t_h, t_w)), np.zeros((t_h, t_w))))
 
 valid_files = set(list(left_offsets.keys())+list(right_offsets.keys()))
-
-temp_files = {}
-for f in os.listdir("data_verified"):
-    if ".jp2" in f:
-        p = f.split("_")
-        if p[0] in valid_files:
-            if p[0] not in temp_files:
-                temp_files[p[0]] = {}
-            if p[1] not in temp_files[p[0]]:
-                temp_files[p[0]][p[1]] = {}
-            temp_files[p[0]][p[1]][p[2]] = f"data_verified/{f}"
-
-bands = {'B02', 'B03', 'B04', 'SCL'}
-files = {}
-for ki,vi in temp_files.items():
-    for kj,vj in vi.items():
-        if len(bands-set(vj.keys())) == 0:
-            if ki not in files:
-                files[ki] = {}
-            if kj not in files[ki]:
-                files[ki][kj] = {}
-            files[ki][kj] = vj
-
-dates = []
-for f in files.values():
-    dates += list(f.keys())
+files = {k:v for k,v in files.items() if k in valid_files}
 
 prev_day = ""
-for d in sorted(list(set(dates))):
+
+for d in dates:
     print(d)
     for k,v in left_offsets.items():
-        if d in files[k] and len(bands-set(files[k][d].keys())) == 0:
+        if d in files[k]:
             fj = files[k][d]
             try:
                 scl = rasterio.open(fj["SCL"], driver="JP2OpenJPEG").read(1)
                 c_red = np.clip(rasterio.open(fj["B04"], driver="JP2OpenJPEG").read(1)*gain/10000, 0, 1)
-                c_green = np.clip(rasterio.open(fj["B03"], driver="JP2OpenJPEG").read(1)*gain/10000, 0, 1)
+                c_green = np.clip(rasterio.open(fj["B03"], driver="JP2OpenJPEG").read(1)*gain/10000, 0, 1) 
                 c_blue = np.clip(rasterio.open(fj["B02"], driver="JP2OpenJPEG").read(1)*gain/10000, 0, 1)
 
                 print(d, k)
                 v_offset = v[0]*(w-overlap)+lt_offset
                 h_offset = v[1]*(w-overlap)
-                mask = ((scl != 0) & (scl != 1) & (scl != 3) & (scl != 7) & (scl != 8) & (scl != 9) & (scl != 10))
-
+                mask = (((scl == 0) | (scl == 1) | (scl == 3) | (scl == 7) | (scl == 8) | (scl == 9) | (scl == 10) | ((c_red == 0) & (c_green == 0) & (c_blue == 0))) != True)
                 composite[v_offset:v_offset+w, h_offset:h_offset+w, 0][mask] = c_red[mask]
                 composite[v_offset:v_offset+w, h_offset:h_offset+w, 1][mask] = c_green[mask]
                 composite[v_offset:v_offset+w, h_offset:h_offset+w, 2][mask] = c_blue[mask]
             except BaseException as e:
                 print(e)
-
-    #if prev_day != d[:8]: # save twice to see if there's jitter
-    #    print("scaling down")
-    #    scaled_down = np.clip(np.dstack((zoom(composite[:,:,0], scale), zoom(composite[:,:,1], scale), zoom(composite[:,:,2], scale))), 0, 1)
-    #    print("saving composite")
-    #    matplotlib.image.imsave(f"{Path.home()}/Projs/bulbulis/true_color_frames/{d[:8]}_left_only.jpeg", scaled_down)
-    #else:
-    #    print(f"skipping save {prev_day} = {d}")
-
+    
     t_scl = np.zeros((bc_h, bc_w))
     did_right = False
     for k,v in right_offsets.items():
-        if d in files[k] and len(bands-set(files[k][d].keys())) == 0:
+        if d in files[k]:
             fj = files[k][d]
             try:
                 scl = rasterio.open(fj["SCL"], driver="JP2OpenJPEG").read(1)
                 c_red = np.clip(rasterio.open(fj["B04"], driver="JP2OpenJPEG").read(1)*gain/10000, 0, 1)
-                c_green = np.clip(rasterio.open(fj["B03"], driver="JP2OpenJPEG").read(1)*gain/10000, 0, 1)
+                c_green = np.clip(rasterio.open(fj["B03"], driver="JP2OpenJPEG").read(1)*gain/10000, 0, 1) 
                 c_blue = np.clip(rasterio.open(fj["B02"], driver="JP2OpenJPEG").read(1)*gain/10000, 0, 1)
 
                 print(d, k)
                 v_offset = v[0]*(w-overlap)
                 h_offset = v[1]*(w-overlap)
-                mask = ((scl != 0) & (scl != 1) & (scl != 3) & (scl != 7) & (scl != 8) & (scl != 9) & (scl != 10))
+                mask = (((scl == 0) | (scl == 1) | (scl == 3) | (scl == 7) | (scl == 8) | (scl == 9) | (scl == 10) | ((c_red == 0) & (c_green == 0) & (c_blue == 0))) != True)
                 right[v_offset:v_offset+w, h_offset:h_offset+w, 0][mask] = c_red[mask]
                 right[v_offset:v_offset+w, h_offset:h_offset+w, 1][mask] = c_green[mask]
                 right[v_offset:v_offset+w, h_offset:h_offset+w, 2][mask] = c_blue[mask]
@@ -141,14 +126,14 @@ for d in sorted(list(set(dates))):
             except BaseException as e:
                 print(e)
     if did_right:
-        mask = ((t_scl == 0) | (t_scl == 1) | (t_scl == 3) | (t_scl == 7) | (t_scl == 8) | (t_scl == 9) | (t_scl == 10))
+        mask = ((t_scl == 0) | (t_scl == 1) | (t_scl == 3) | (t_scl == 7) | (t_scl == 8) | (t_scl == 9) | (t_scl == 10) | ((right[:,:,0] == 0) & (right[:,:,1] == 0) & (right[:,:,2] == 0)))
         t_scl[mask] = -999 # rotation ends up interpolating values - need something exteme so that I can still tell where the clouds are
-
+        
         temp_right = rotate(right, deg, reshape=False)
         t_scl = rotate(t_scl, deg, reshape=False)
         mask = ((t_scl > 0))
         composite[:bc_h, bc_w-bc_offset:, :][mask] = temp_right[mask] # no point in clipping here since I'll need to clip after zooming anyhow
-
+    
     if prev_day != d[:8]:
         prev_day = d[:8]
         print("scaling down")
